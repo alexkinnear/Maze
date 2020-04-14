d14b0c7f6b4ac106f5f73b4c2e3410a80f84cfc1
Alex Kinnear <alexander.kinnear@aggiemail.usu.edu>
2020-04-14 01:20:06 -0600

    Black pixels being unproperly identified

--- src/maze.java
+++ src/maze.java
@@ -3,10 +3,9 @@ import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 
 
+import javafx.beans.InvalidationListener;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelReader;
+import javafx.scene.image.*;
 import javafx.scene.input.KeyCode;
 
 import javafx.scene.layout.Pane;
@@ -21,6 +20,7 @@ import javafx.util.Duration;
 import java.util.Scanner;
 
 public class maze extends Application {
+    int movement = 5;
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -31,19 +31,39 @@ public class maze extends Application {
         int choice = scan.nextInt();
 
         Image image = getImage(choice);
+        WritableImage image2 = new WritableImage((int)image.getWidth(), (int)image.getHeight());
+        ImageView img2 = new ImageView(image2);
+        PixelReader pr = image.getPixelReader();
+        PixelWriter pw = image2.getPixelWriter();
+
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                if (!pr.getColor(x, y).equals(Color.WHITE)) {
+                    pw.setColor(x, y, Color.RED);
+                }
+            }
+        }
+
         ImageView maze = getMaze(choice);
-        pane.getChildren().addAll(maze);
+        pane.getChildren().addAll(maze, img2);
+
+
 
         Rectangle r = getRectangle(choice);
         r.setFill(Color.RED);
+
         pane.getChildren().add(r);
+
+
 
         Scene scene = new Scene(pane, 800, 800);
 
-        move(scene, r);
+        move(scene, r, image);
+
 
         stage.setScene(scene);
         stage.show();
+
     }
 
     private Image getImage(int choice) {
@@ -51,7 +71,7 @@ public class maze extends Application {
             case 1: return new Image("File:img/Maze_1.jpg");
             case 2: return new Image("File:img/Maze_2.jpg");
             case 3: return new Image("File:img/Maze_3.jpg");
-            default: return new Image("File:img/Maze_4.png");
+            default: return new Image("File:img/Maze_4.jpg");
         }
     }
 
@@ -77,7 +97,7 @@ public class maze extends Application {
         maze3.setX(50);
         maze3.setY(50);
 
-        Image Maze_4 = new Image("File:img/Maze_4.png");
+        Image Maze_4 = new Image("File:img/Maze_4.jpg");
         ImageView maze4 = new ImageView(Maze_4);
         maze4.setFitWidth(700);
         maze4.setFitHeight(700);
@@ -94,45 +114,68 @@ public class maze extends Application {
 
     private Rectangle getRectangle(int choice) {
         switch(choice) {
-            case 1: return new Rectangle(350, 30, 20, 20);
+            case 1: return new Rectangle(350, 30, 10, 10);
             case 2: return new Rectangle(120, 147, 10, 10);
             case 3: return new Rectangle(65, 697, 10, 10);
-            default: return new Rectangle(60, 185, 20, 20);
+            default: return new Rectangle(60, 185, 10, 10);
         }
     }
 
 
 
 
-    private void move(Scene scene, Rectangle r) {
+    private void move(Scene scene, Rectangle r, Image image) {
+        PixelReader pr = image.getPixelReader();
+
+
         scene.setOnKeyPressed(e -> {
             if (e.getCode().equals(KeyCode.LEFT)) {
                 TranslateTransition translate = new TranslateTransition();
-                translate.setByX(-10);
+                translate.setByX(-movement);
+                r.setX(r.getX() - movement);
                 translate.setDuration(Duration.seconds(0.05));
                 translate.setNode(r);
                 translate.play();
+
+                if (!pr.getColor((int)r.getX(), (int)r.getY()).equals(Color.WHITE)) {
+                    System.out.println("Black");
+                }
             }
             if (e.getCode().equals(KeyCode.RIGHT)) {
                 TranslateTransition translate = new TranslateTransition();
-                translate.setByX(10);
-                translate.setDuration(Duration.seconds(0.05));
+                translate.setByX(movement);
+                r.setX(r.getX() + movement);
+                translate.setDuration(Duration.seconds(0.005));
                 translate.setNode(r);
                 translate.play();
+
+                if (!pr.getColor((int)r.getX(), (int)r.getY()).equals(Color.WHITE)) {
+                    System.out.println("Black");
+                }
             }
             if (e.getCode().equals(KeyCode.UP)) {
                 TranslateTransition translate = new TranslateTransition();
-                translate.setByY(-10);
+                translate.setByY(-movement);
+                r.setY(r.getY() - movement);
                 translate.setDuration(Duration.seconds(0.05));
                 translate.setNode(r);
                 translate.play();
+
+                if (!pr.getColor((int)r.getX(), (int)r.getY()).equals(Color.WHITE)) {
+                    System.out.println("Black");
+                }
             }
             if (e.getCode().equals(KeyCode.DOWN)) {
                 TranslateTransition translate = new TranslateTransition();
-                translate.setByY(10);
+                translate.setByY(movement);
+                r.setY(r.getY() + movement);
                 translate.setDuration(Duration.seconds(0.05));
                 translate.setNode(r);
                 translate.play();
+
+                if (!pr.getColor((int)r.getX(), (int)r.getY()).equals(Color.WHITE)) {
+                    System.out.println("Black");
+                }
             }
         });
     }
